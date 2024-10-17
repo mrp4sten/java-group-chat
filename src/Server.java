5f0865ffe553f3feef06b4ba299456fde86e207c
mavor <mauricio.pasten.martinez@gmail.com>
2024-10-16 22:48:49 -0600

    adding void method and fix output parameter for ClientHandler constructor

--- src/Server.java
+++ src/Server.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
     private ServerSocket serverSocket;
@@ -11,9 +12,9 @@ public class Server {
     public void start() {
         try {
             while (!serverSocket.isClosed()) {
-                serverSocket.accept();
+                Socket socket = serverSocket.accept();
                 System.out.println("Client connected");
-                ClientHandler clientHandler = new ClientHandler();
+                ClientHandler clientHandler = new ClientHandler(socket);
 
                 Thread clientThread = new Thread(clientHandler);
                 clientThread.start();
@@ -25,9 +26,19 @@ public class Server {
 
     public void close() {
         try {
-            if(serverSocket != null) {
+            if (serverSocket != null) {
                 serverSocket.close();
             }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(3000);
+            Server server = new Server(serverSocket);
+            server.start();
         } catch (IOException e) {
             e.printStackTrace();
         }
